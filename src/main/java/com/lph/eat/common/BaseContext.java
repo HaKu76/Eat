f2a8acf604aaef9aa051cbcf281e7167795d3302
HaKu76 <2505302304@qq.com>
2024-07-18 18:27:05 +0800

    基于ThreadLocal封装工具类

---
+++ src/main/java/com/lph/eat/common/BaseContext.java
@@ -0,0 +1,26 @@
+package com.lph.eat.common;
+
+/**
+ * 基于ThreadLocal封装工具类，用户保存和获取当前登录用户id
+ * 以线程为作用域，每个线程保存自己的副本
+ * 例如每次获取id时，从当前线程获取是同一个线程同一个id
+ */
+public class BaseContext {
+    private static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    /**
+     * 设置值
+     * @param id
+     */
+    public static void setCurrentId(Long id) {
+        threadLocal.set(id);
+    }
+
+    /**
+     * 获取值
+     * @return
+     */
+    public static Long getCurrentId() {
+        return threadLocal.get();
+    }
+}
