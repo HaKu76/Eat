676ee2d8480d3c3a4f5c2a588bce7f6d28ba98b9
HaKu76 <2505302304@qq.com>
2024-07-18 18:26:55 +0800

    员工实体类

--- src/main/java/com/lph/eat/entity/Employee.java
+++ src/main/java/com/lph/eat/entity/Employee.java
@@ -3,11 +3,13 @@ package com.lph.eat.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
+
 /*
-* 员工实体类
-*/
+ * 员工实体类
+ */
 @Data
 public class Employee implements Serializable {
 
@@ -24,19 +26,20 @@ public class Employee implements Serializable {
     private String phone;
 
     private String sex;
-    // 身份证号
-    private String idNumber;
+
+    private String idNumber;//身份证号码
 
     private Integer status;
 
+    @TableField(fill = FieldFill.INSERT) //插入时填充字段
     private LocalDateTime createTime;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE) //插入和更新时填充字段
     private LocalDateTime updateTime;
 
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(fill = FieldFill.INSERT) //插入时填充字段
     private Long createUser;
 
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(fill = FieldFill.INSERT_UPDATE) //插入和更新时填充字段
     private Long updateUser;
-
 }
