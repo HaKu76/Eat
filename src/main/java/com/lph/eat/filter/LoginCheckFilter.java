61ed19ef3192fdfaf9cf418a7d30c022f6d7451e
HaKu76 <2505302304@qq.com>
2024-07-18 18:25:30 +0800

    登录过滤器

--- src/main/java/com/lph/eat/filter/LoginCheckFilter.java
+++ src/main/java/com/lph/eat/filter/LoginCheckFilter.java
@@ -1,6 +1,7 @@
 package com.lph.eat.filter;
 
 import com.alibaba.fastjson.JSON;
+import com.lph.eat.common.BaseContext;
 import com.lph.eat.common.req;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.AntPathMatcher;
@@ -20,6 +21,7 @@ import java.io.IOException;
 public class LoginCheckFilter implements Filter {
     // 路径匹配器，支持通配符
     public static final AntPathMatcher pathMatcher = new AntPathMatcher();
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
@@ -29,7 +31,7 @@ public class LoginCheckFilter implements Filter {
         // 1.获取uri
         String requestURI = request.getRequestURI();
 
-        log.info("拦截到请求：{}",requestURI);
+        log.info("拦截到请求：{}", requestURI);
         // 2.判断请求是否要处理，这些是不需要处理的请求路径
         String[] urls = new String[]{
                 "/employee/login",
@@ -40,15 +42,19 @@ public class LoginCheckFilter implements Filter {
 
         boolean check = check(urls, requestURI);
         // 3.不需要处理则放行
-        if (check){
-            log.info("本次请求{}不需要处理",requestURI);
-            filterChain.doFilter(request,response);
+        if (check) {
+            log.info("本次请求{}不需要处理", requestURI);
+            filterChain.doFilter(request, response);
             return;
         }
         // 4.判断登录状态，已经登录则放行
-        if (request.getSession().getAttribute("employee") != null){
-            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
-            filterChain.doFilter(request,response);
+        if (request.getSession().getAttribute("employee") != null) {
+            log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("employee"));
+            // 调用BaseContext设置当前登录用户id
+            long empId = (Long) request.getSession().getAttribute("employee");
+            BaseContext.setCurrentId(empId);
+
+            filterChain.doFilter(request, response);
             return;
         }
         log.info("用户未登录");
@@ -59,14 +65,15 @@ public class LoginCheckFilter implements Filter {
 
     /**
      * 路径匹配，检查本次请求是否要放行
+     *
      * @param urls
      * @param requestURI
      * @return
      */
-    public boolean check(String[] urls, String requestURI){
+    public boolean check(String[] urls, String requestURI) {
         for (String url : urls) {
             boolean match = pathMatcher.match(url, requestURI);
-            if (match){
+            if (match) {
                 return true;
             }
         }
