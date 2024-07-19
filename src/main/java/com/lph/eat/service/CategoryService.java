af42f300af6884173db059c7c7b4a58ae870683a
HaKu76 <2505302304@qq.com>
2024-07-19 19:02:36 +0800

    分类服务类接口

---
+++ src/main/java/com/lph/eat/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.lph.eat.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lph.eat.entity.Category;
+
+/**
+ * 分类服务类接口
+ */
+public interface CategoryService extends IService<Category> {
+}
