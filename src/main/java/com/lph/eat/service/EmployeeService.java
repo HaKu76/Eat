50ce8ac6fd9e386c751b0b7c3d40127902999b06
HaKu76 <2505302304@qq.com>
2024-07-19 19:02:18 +0800

    员工服务类接口

--- src/main/java/com/lph/eat/service/EmployeeService.java
+++ src/main/java/com/lph/eat/service/EmployeeService.java
@@ -2,6 +2,6 @@ package com.lph.eat.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.lph.eat.entity.Employee;
-
+// 员工服务类接口
 public interface EmployeeService extends IService<Employee> {
 }
