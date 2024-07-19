b62de114626fa1487b75c88fa945d86b638d00d4
HaKu76 <2505302304@qq.com>
2024-07-19 20:02:22 +0800

    套餐服务类接口

---
+++ src/main/java/com/lph/eat/service/SetmealService.java
@@ -0,0 +1,10 @@
+package com.lph.eat.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lph.eat.entity.Setmeal;
+
+/**
+ * 套餐服务类接口
+ */
+public interface SetmealService extends IService<Setmeal> {
+}
