154b422717002f6e9f57900a2c980000d70cad28
HaKu76 <2505302304@qq.com>
2024-07-19 19:03:25 +0800

    员工Mapper接口

--- src/main/java/com/lph/eat/mapper/EmployeeMapper.java
+++ src/main/java/com/lph/eat/mapper/EmployeeMapper.java
@@ -3,7 +3,7 @@ package com.lph.eat.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lph.eat.entity.Employee;
 import org.apache.ibatis.annotations.Mapper;
-
+// 员工Mapper接口
 @Mapper
 public interface EmployeeMapper extends BaseMapper<Employee> {
 }
