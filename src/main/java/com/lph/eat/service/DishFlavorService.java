ab6fd4e22c0d8e71ba4c4d714e22fd55072921ff
HaKu76 <2505302304@qq.com>
2024-07-20 20:45:44 +0800

    菜品口味服务类接口

---
+++ src/main/java/com/lph/eat/service/DishFlavorService.java
@@ -0,0 +1,10 @@
+package com.lph.eat.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lph.eat.entity.DishFlavor;
+
+/**
+ * 菜品口味服务类接口
+ */
+public interface DishFlavorService extends IService<DishFlavor> {
+}
