d49231a51f5d3d40b83c633c4309f37879449a97
HaKu76 <2505302304@qq.com>
2024-07-20 20:45:36 +0800

    菜品口味Mapper接口

---
+++ src/main/java/com/lph/eat/mapper/DishFlavorMapper.java
@@ -0,0 +1,12 @@
+package com.lph.eat.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lph.eat.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 菜品口味Mapper接口
+ */
+@Mapper
+public interface DishFlavorMapper extends BaseMapper<DishFlavor> {
+}
