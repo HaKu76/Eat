71755194e829fa6c3a0659f7a87f00bfd26a5b44
HaKu76 <2505302304@qq.com>
2024-07-25 01:44:49 +0800

    套餐服务实现类

--- src/main/java/com/lph/eat/service/impl/SetmealServiceImpl.java
+++ src/main/java/com/lph/eat/service/impl/SetmealServiceImpl.java
@@ -1,14 +1,73 @@
 package com.lph.eat.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lph.eat.common.CustomException;
+import com.lph.eat.dto.SetmealDto;
 import com.lph.eat.entity.Setmeal;
+import com.lph.eat.entity.SetmealDish;
 import com.lph.eat.mapper.SetmealMapper;
+import com.lph.eat.service.SetmealDishService;
 import com.lph.eat.service.SetmealService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 套餐服务实现类
  */
 @Service
 public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements SetmealService {
+    @Autowired
+    private SetmealDishService setmealDishService;
+
+    /**
+     * 新增套餐
+     *
+     * @param setmealDto
+     */
+    // 因为有两张表，所以要保存事务的一致性
+    @Transactional
+    public void saveWithDish(SetmealDto setmealDto) {
+        // 保存套餐的基本信息，操作setmeal，执行insert操作
+        this.save(setmealDto);
+        List<SetmealDish> setmealDishes = setmealDto.getSetmealDishes();
+        setmealDishes.stream().map((item) -> {
+            item.setSetmealId(setmealDto.getId());
+            return item;
+        }).collect(Collectors.toList());
+        // 保存套餐和菜品的关联信息，操作setmeal_dish，执行insert操作
+        setmealDishService.saveBatch(setmealDishes);
+    }
+
+
+    /**
+     * 删除套餐，同时删除套餐和菜品的关联数据
+     *
+     * @param ids
+     */
+    @Transactional
+    public void removeWithDish(List<Long> ids) {
+        // 查询套餐状态，确定是否可以删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        // 根据id查询套餐
+        queryWrapper.in(Setmeal::getId, ids);
+        // 判断状态是否停售
+        queryWrapper.eq(Setmeal::getStatus, 1);
+
+        int count = this.count(queryWrapper);
+        // 如果存在套餐在售，不能删除，抛出异常
+        if (count > 0) {
+            throw new CustomException("套餐正在售卖中，不能删除");
+        }
+        // 如果可以删除，先删除套餐表中的数据
+        this.removeByIds(ids);
+        LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.in(SetmealDish::getSetmealId, ids);
+        // 删除关系表中的数据，即删除套餐和菜品的关联数据，setmeal_dish
+        setmealDishService.remove(lambdaQueryWrapper);
+    }
 }
