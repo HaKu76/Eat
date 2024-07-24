f8cc79c1c84b4a2d5dd903bf02345998e848defd
HaKu76 <2505302304@qq.com>
2024-07-24 14:36:57 +0800

    菜品服务实现类

--- src/main/java/com/lph/eat/service/impl/DishServiceImpl.java
+++ src/main/java/com/lph/eat/service/impl/DishServiceImpl.java
@@ -1,14 +1,49 @@
 package com.lph.eat.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lph.eat.dto.DishDto;
 import com.lph.eat.entity.Dish;
+import com.lph.eat.entity.DishFlavor;
 import com.lph.eat.mapper.DishMapper;
+import com.lph.eat.service.DishFlavorService;
 import com.lph.eat.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品服务实现类
  */
 @Service
+
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+    /**
+     * 新增菜品，同时保存口味
+     *
+     * @param dishDto
+     */
+    // 事务控制注解
+    @Transactional
+    public void saveWithFlavor(DishDto dishDto) {
+        //保存菜品的基本信息到菜品表dish
+        this.save(dishDto);
+
+        Long dishId = dishDto.getId();//菜品id
+
+        // 菜品口味
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors = flavors.stream().map((item) -> {
+            item.setDishId(dishId);
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存菜品口味到菜品口味表dish_flavor
+        dishFlavorService.saveBatch(flavors);
+    }
 }
