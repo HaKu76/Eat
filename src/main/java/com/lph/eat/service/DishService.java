5dc5dc4d050a232f4933b31e9b7266cb792c2ece
HaKu76 <2505302304@qq.com>
2024-07-24 14:36:46 +0800

    菜品服务类接口

--- src/main/java/com/lph/eat/service/DishService.java
+++ src/main/java/com/lph/eat/service/DishService.java
@@ -1,10 +1,13 @@
 package com.lph.eat.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.lph.eat.dto.DishDto;
 import com.lph.eat.entity.Dish;
 
 /**
  * 菜品服务类接口
  */
 public interface DishService extends IService<Dish> {
+    //新增菜品，同时插入菜品对应的口味数据，需要操作两张表：dish、dish_flavor
+    public void saveWithFlavor(DishDto dishDto);
 }
