aba57cf8dd5209f05fe3ffaa438fa4628025b3a8
HaKu76 <2505302304@qq.com>
2024-07-24 14:36:36 +0800

    关联查询菜品和口味

---
+++ src/main/java/com/lph/eat/dto/DishDto.java
@@ -0,0 +1,21 @@
+package com.lph.eat.dto;
+
+import com.lph.eat.entity.Dish;
+import com.lph.eat.entity.DishFlavor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 关联查询菜品和口味
+ */
+@Data
+public class DishDto extends Dish {
+
+    private List<DishFlavor> flavors = new ArrayList<>();
+
+    private String categoryName;
+
+    private Integer copies;
+}
