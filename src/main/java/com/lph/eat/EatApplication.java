c88ef0d35dc632eee0f2fec1579a3c66c86de0e3
HaKu76 <2505302304@qq.com>
2024-07-24 14:37:15 +0800

    启动类

--- src/main/java/com/lph/eat/EatApplication.java
+++ src/main/java/com/lph/eat/EatApplication.java
@@ -4,11 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @SpringBootApplication
 // 过滤器扫描
 @ServletComponentScan
+// 开启事务
+@EnableTransactionManagement
 public class EatApplication {
 
     public static void main(String[] args) {
