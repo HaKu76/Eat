a5dc19385fcf27dde076c60e4aa7c913ef96d225
HaKu76 <2505302304@qq.com>
2024-07-25 01:43:33 +0800

    套餐菜品Mapper接口

---
+++ src/main/java/com/lph/eat/mapper/SetmealDishMapper.java
@@ -0,0 +1,12 @@
+package com.lph.eat.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lph.eat.entity.SetmealDish;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 套餐菜品Mapper接口
+ */
+@Mapper
+public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
+}
