df365aef1b406f801e39930ce495ddf897159b34
HaKu76 <2505302304@qq.com>
2024-07-26 15:59:54 +0800

    用户信息服务类接口

---
+++ src/main/java/com/lph/eat/service/UserService.java
@@ -0,0 +1,10 @@
+package com.lph.eat.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lph.eat.entity.User;
+
+/**
+ * 用户信息服务类接口
+ */
+public interface UserService extends IService<User> {
+}
