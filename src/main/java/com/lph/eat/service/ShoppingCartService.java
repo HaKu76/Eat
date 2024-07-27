b880e3c80b2d8f744ba7cf1f7fc9cc1884ee97c8
HaKu76 <2505302304@qq.com>
2024-07-27 14:35:28 +0800

    购物车服务类接口

---
+++ src/main/java/com/lph/eat/service/ShoppingCartService.java
@@ -0,0 +1,11 @@
+package com.lph.eat.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lph.eat.entity.ShoppingCart;
+
+/**
+ * 购物车服务类接口
+ */
+public interface ShoppingCartService extends IService<ShoppingCart> {
+
+}
