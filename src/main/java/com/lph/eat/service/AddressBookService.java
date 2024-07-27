f455b1716e6bab87c738a6b64b767ea0f9ed8b5a
HaKu76 <2505302304@qq.com>
2024-07-27 14:30:25 +0800

    地址簿服务类接口

---
+++ src/main/java/com/lph/eat/service/AddressBookService.java
@@ -0,0 +1,11 @@
+package com.lph.eat.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lph.eat.entity.AddressBook;
+
+/**
+ * 地址簿服务类接口
+ */
+public interface AddressBookService extends IService<AddressBook> {
+
+}
